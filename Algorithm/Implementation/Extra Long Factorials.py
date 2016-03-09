1bc21ee4c401323b7c62fc69ecc0b71f9dcec8f1
bijandakal <dhakal.bijan@gmail.com>
2016-03-09 23:02:12 +0545

    Extra Long Factorials

---
+++ Algorithm/Implementation/Extra Long Factorials.py
@@ -0,0 +1,10 @@
+__author__ = 'bijan'
+n = int(raw_input().strip())
+
+def factorial(n):
+    if (n ==  0 or n == 1 ):
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print factorial(n)
